b55d207042d9c83655f53cc04cc96f6326cb6e3d
Ramsey Darling <rdarling42@gmail.com>
2015-05-27 19:30:10 -0400

    putting back js for verification

--- application/views/elements/outside_footer.php
+++ application/views/elements/outside_footer.php
@@ -14,10 +14,10 @@
     </div>
 </div>
 </div><!--end class container-->
-	<!--<script src="/assets/js/register.js"></script>-->
 <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="/assets/bower_components/materialize/dist/js/materialize.min.js"></script>
+	<script src="/assets/js/register.js"></script>
 	<script>
 	$(document).ready(function(){
 		$('#couple_checkbox').click(function(){
